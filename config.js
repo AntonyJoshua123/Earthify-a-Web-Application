d7cf85cfc1a6aead0d189154f346bafbc376a33e
15Harish <harish.27it@licet.ac.in>
2025-02-22 10:17:12 +0530

    hchanges

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDDYp7vmd_cz0YObFTI9rlOgHkh9m93GXo",
+    authDomain: "login-page-15ae5.firebaseapp.com",
+    databaseURL: "https://login-page-15ae5-default-rtdb.firebaseio.com",
+    projectId: "login-page-15ae5",
+    storageBucket: "login-page-15ae5.firebasestorage.app",
+    messagingSenderId: "391349086899",
+    appId: "1:391349086899:web:a29e22298537bb64d99ec6"
+};
+
+export default firebaseConfig;
--- register.js
+++ register.js
@@ -6,15 +6,7 @@ import { getDatabase, ref, set, serverTimestamp } from "https://www.gstatic.com/
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-   apiKey: "AIzaSyDDYp7vmd_cz0YObFTI9rlOgHkh9m93GXo",
-   authDomain: "login-page-15ae5.firebaseapp.com",
-   databaseURL: "https://login-page-15ae5-default-rtdb.firebaseio.com",
-   projectId: "login-page-15ae5",
-   storageBucket: "login-page-15ae5.firebasestorage.app",
-   messagingSenderId: "391349086899",
-   appId: "1:391349086899:web:a29e22298537bb64d99ec6"
-};
+import firebaseConfig from "./config.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -44,7 +36,7 @@ submit.addEventListener("click", function (event) {
        });
    })
    .then(() => {
-       alert("Account Created Successfully!");
+       
        window.location.href = "main.html";  
     })
    .catch((error) => {
